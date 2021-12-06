a8f1cee86248684888522172ccf0892eb4a4d58f
Mahamud Arteh <Mahamud.Arteh@clubsystems.com>
2021-12-06 09:27:19 +0000

    Add project files.

---
+++ csharp-records/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_records
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
